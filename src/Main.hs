9b054fa4786e1f3f53c503e7314ee9cc6de8af79
Jan Hrcek <jhrcek@redhat.com>
2017-11-21 14:38:43 +0100

    Fail on missing cmdline arg

--- src/Main.hs
+++ src/Main.hs
@@ -11,13 +11,15 @@ import Data.Text.IO as T
 import Data.Time.Clock (DiffTime)
 import Data.Time.Format (defaultTimeLocale, parseTimeM)
 import Data.Time.LocalTime (timeOfDayToTime, timeToTimeOfDay)
+import System.Directory (doesFileExist)
 import System.Environment (getArgs)
+import System.Exit (die)
 import Text.HTML.TagSoup (Tag, fromTagText, isTagText, parseTags, partitions)
 import Text.HTML.TagSoup.Match (tagOpenAttrLit)
 
 main :: IO ()
 main = do
-    [infile] <- getArgs
+    infile <- parseArgs
     input <- T.readFile infile
     let pluginSummaries = process input
     --mapM_ print pluginSummaries
@@ -34,18 +36,18 @@ data LogEntry = LogEntry DiffTime Text deriving Show
 type PluginEntries = [LogEntry]
 
 data PluginSummary = PluginSummary
-    { pluginName ::Text
+    { pluginName :: Text
     , execution  :: Text
-    , artifactId ::Text
+    , artifactId :: Text
     , logText    :: Text
     , duration   :: DiffTime
     } deriving Show
 
 entriesToSummary :: PluginEntries -> PluginSummary
 entriesToSummary entries = PluginSummary
-    { pluginName = pluginName
-    , execution = execution
-    , artifactId = artifactId
+    { pluginName = pluginName'
+    , execution = execution'
+    , artifactId = artifactId'
     , logText = Text.unlines $ map getPayload entries
     , duration = end - start
     }
@@ -54,11 +56,11 @@ entriesToSummary entries = PluginSummary
     (LogEntry end _    ) = last entries
     -- Break "[INFO] --- maven-resources-plugin:3.0.2:resources (default-resources) @ kjar-with-instrumentation" into
     -- into            ("maven-resources-plugin:3.0.2:resources", "(default-resources)", "kjar-with-instrumentation")
-    (pluginName, execution, artifactId) = case break (=="@") $ Text.words txt of
+    (pluginName', execution', artifactId') = case break (=="@") $ Text.words txt of
         (beforeAtSign, [_,art,_]) ->
             let [plugin, exec] = drop (length beforeAtSign - 2) beforeAtSign
-            in (plugin, execution, art)
-        xs -> error . Text.unpack $ "unexpected format " <> txt
+            in (plugin, exec, art)
+        _ -> error . Text.unpack $ "unexpected format " <> txt
 
 getPayload :: LogEntry -> Text
 getPayload (LogEntry _ payload) = payload
@@ -83,3 +85,16 @@ parseTimestamp ts =
   case parseTimeM False defaultTimeLocale "%H:%M:%S" (Text.unpack ts) of
     Just tod -> timeOfDayToTime tod
     Nothing  -> error $ "Failed to parse timestamp " ++ show ts
+
+parseArgs :: IO FilePath
+parseArgs = do
+  args <- getArgs
+  case args of
+    [infile] -> do
+       exists <- doesFileExist infile
+       if exists then return infile
+                 else dieOnInvalidInput
+    _ -> dieOnInvalidInput
+
+dieOnInvalidInput ::  IO a
+dieOnInvalidInput = die "Please supply file containing consoleFull text of jenkins job to analyze"
